4202ed9c0faf4ef208de5a93f16709831925cc03
Anil Pal <anilpal.pal5@gmail.com>
2024-01-01 07:34:29 +0530

    feat: adding framer motion for animation

--- src/TodoHome/CompletedTodos.tsx
+++ src/TodoHome/CompletedTodos.tsx
@@ -2,6 +2,7 @@ import { Box, Callout, Flex, Heading, IconButton } from '@radix-ui/themes'
 import { useTodos } from '../hooks/useTodos'
 import { Pencil2Icon, ResetIcon, TrashIcon } from '@radix-ui/react-icons'
 import { Todo } from './types'
+import { AnimatePresence, motion } from 'framer-motion'
 
 const CompletedTodos = () => {
   const { getAllTodosQuery, deleteTodoMutation, updateTodoMutation } =
@@ -33,43 +34,56 @@ const CompletedTodos = () => {
         </Heading>
       </Flex>
       <Flex direction="column" gap="2">
-        {completedTodos?.map((todo, index) => (
-          <Flex justify="between" align="center" gap="3" key={index}>
-            <Callout.Root
-              size="1"
-              variant="soft"
-              color="green"
-              style={{ flexGrow: 1 }}
+        <AnimatePresence>
+          {completedTodos?.map((todo) => (
+            <motion.div
+              style={{
+                display: 'flex',
+                justifyContent: 'space-between',
+                alignItems: 'center',
+                gap: '12px',
+              }}
+              key={todo.id}
+              initial={{ opacity: 0, scale: 1 }}
+              animate={{ opacity: 1, scale: 1 }}
+              exit={{ opacity: 0, scale: 0.5 }}
             >
-              <Callout.Icon>
-                <Pencil2Icon />
-              </Callout.Icon>
-              <Callout.Text>{todo.text}</Callout.Text>
-            </Callout.Root>
-            <Flex gap="2">
-              <IconButton
-                color="amber"
-                variant="surface"
-                size="3"
-                onClick={() => {
-                  handleRevertTodoClick(todo)
-                }}
+              <Callout.Root
+                size="1"
+                variant="soft"
+                color="green"
+                style={{ flexGrow: 1 }}
               >
-                <ResetIcon />
-              </IconButton>
-              <IconButton
-                color="red"
-                variant="outline"
-                size="3"
-                onClick={() => {
-                  handleDeleteTodoClick(todo)
-                }}
-              >
-                <TrashIcon />
-              </IconButton>
-            </Flex>
-          </Flex>
-        ))}
+                <Callout.Icon>
+                  <Pencil2Icon />
+                </Callout.Icon>
+                <Callout.Text>{todo.text}</Callout.Text>
+              </Callout.Root>
+              <Flex gap="2">
+                <IconButton
+                  color="amber"
+                  variant="surface"
+                  size="3"
+                  onClick={() => {
+                    handleRevertTodoClick(todo)
+                  }}
+                >
+                  <ResetIcon />
+                </IconButton>
+                <IconButton
+                  color="red"
+                  variant="outline"
+                  size="3"
+                  onClick={() => {
+                    handleDeleteTodoClick(todo)
+                  }}
+                >
+                  <TrashIcon />
+                </IconButton>
+              </Flex>
+            </motion.div>
+          ))}
+        </AnimatePresence>
       </Flex>
     </Box>
   )
--- src/TodoHome/OpenTodos.tsx
+++ src/TodoHome/OpenTodos.tsx
@@ -2,6 +2,7 @@ import { Box, Callout, Flex, IconButton } from '@radix-ui/themes'
 import { useTodos } from '../hooks/useTodos'
 import { CheckIcon, Pencil2Icon, TrashIcon } from '@radix-ui/react-icons'
 import { Todo } from './types'
+import { AnimatePresence, motion } from 'framer-motion'
 
 const OpenTodos = () => {
   const { getAllTodosQuery, updateTodoMutation, deleteTodoMutation } =
@@ -24,38 +25,51 @@ const OpenTodos = () => {
   return (
     <Box my="5">
       <Flex direction="column" gap="2">
-        {openTodos?.map((todo, index) => (
-          <Flex justify="between" align="center" gap="3" key={index}>
-            <Callout.Root size="1" variant="soft" style={{ flexGrow: 1 }}>
-              <Callout.Icon>
-                <Pencil2Icon />
-              </Callout.Icon>
-              <Callout.Text>{todo.text}</Callout.Text>
-            </Callout.Root>
-            <Flex gap="2">
-              <IconButton
-                color="grass"
-                variant="surface"
-                size="3"
-                onClick={() => {
-                  handleCompleteTodoClick(todo)
-                }}
-              >
-                <CheckIcon />
-              </IconButton>
-              <IconButton
-                color="red"
-                variant="outline"
-                size="3"
-                onClick={() => {
-                  handleDeleteTodoClick(todo)
-                }}
-              >
-                <TrashIcon />
-              </IconButton>
-            </Flex>
-          </Flex>
-        ))}
+        <AnimatePresence>
+          {openTodos?.map((todo) => (
+            <motion.div
+              style={{
+                display: 'flex',
+                justifyContent: 'space-between',
+                alignItems: 'center',
+                gap: '12px',
+              }}
+              key={todo.id}
+              initial={{ opacity: 0, scale: 1 }}
+              animate={{ opacity: 1, scale: 1 }}
+              exit={{ opacity: 0, scale: 0.5 }}
+            >
+              <Callout.Root size="1" variant="soft" style={{ flexGrow: 1 }}>
+                <Callout.Icon>
+                  <Pencil2Icon />
+                </Callout.Icon>
+                <Callout.Text>{todo.text}</Callout.Text>
+              </Callout.Root>
+              <Flex gap="2">
+                <IconButton
+                  color="grass"
+                  variant="surface"
+                  size="3"
+                  onClick={() => {
+                    handleCompleteTodoClick(todo)
+                  }}
+                >
+                  <CheckIcon />
+                </IconButton>
+                <IconButton
+                  color="red"
+                  variant="outline"
+                  size="3"
+                  onClick={() => {
+                    handleDeleteTodoClick(todo)
+                  }}
+                >
+                  <TrashIcon />
+                </IconButton>
+              </Flex>
+            </motion.div>
+          ))}
+        </AnimatePresence>
       </Flex>
     </Box>
   )
